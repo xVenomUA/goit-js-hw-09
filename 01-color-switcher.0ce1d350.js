27e40d0cef251cbbf523ea170254a0687a7d107c
xVenomUA <117669302+xVenomUA@users.noreply.github.com>
2023-10-22 11:22:38 +0000

    Deploying to gh-pages from @ xVenomUA/goit-js-hw-09@41ef8ae41cfcacedf9c6abd9bb59c468ea9bcf1a 🚀

--- 01-color-switcher.0ce1d350.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-stop]"),r=document.querySelector("button[data-start]");let o=null;r.addEventListener("click",(()=>{o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),3e3),r.setAttribute("disabled",!0)}));e.addEventListener("click",(()=>{clearInterval(o),r.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.0ce1d350.js.map
