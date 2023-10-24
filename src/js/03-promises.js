791f195964b907ddcf186e5d243d0f836d6c3f04
Pohrebniak Andrian <pogra29@gmail.com>
2023-10-24 23:38:36 +0300

    ff

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
+import Notiflix from 'notiflix';
+const btnform = document.querySelector('form');
+const options = {
+  timeout: 5000,
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+function onclick(evt) {
+  evt.preventDefault();
+  const delay = Number(evt.target.delay.value);
+  const step = Number(evt.target.step.value);
+  const amount = Number(evt.target.amount.value);
+  if (delay < 0 || step  < 0 ) {
+    Notiflix.Notify.info('DELAY AND STEP ONLY > 0 ', options);
+    return;
+  }
+  if (amount <= 0) { 
+    Notiflix.Notify.info('AMOUNT HAVE TO > 0', options);
+    return;
+  }
+  for (let i = 0; i < amount; i += 1) {
+    let position = i + 1;
+    let alldelay = i * step + delay;
+    console.log(alldelay);
+    createPromise(position, alldelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay} ms`,
+          options
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay} ms`,
+          options
+        );
+      });
+  }
+  form.reset();
+}
+btnform.addEventListener('submit', onclick);
